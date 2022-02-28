8e01717df0a03472e9e26e30aebd2789d399f242
Madson Silva <madsonoliveirasilva@gmail.com>
2022-02-28 16:13:47 -0300

    feat: add seminarios

---
+++ src/api/seminario/controllers/seminario.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  seminario controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::seminario.seminario');
---
+++ src/api/seminario/routes/seminario.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * seminario router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::seminario.seminario');
