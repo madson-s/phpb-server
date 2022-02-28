4e4ab7fe5da1b7c1c204970915f8384ce0c16941
Madson Silva <madsonoliveirasilva@gmail.com>
2022-02-28 16:15:59 -0300

    feat: add producões

---
+++ src/api/producao/controllers/producao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  producao controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::producao.producao');
---
+++ src/api/producao/routes/producao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * producao router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::producao.producao');
