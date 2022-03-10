744527c781e6d2fb3034ec28bd6ca7c31129b72a
Madson Silva <madsonoliveirasilva@gmail.com>
2022-03-09 22:12:31 -0300

    fix: ensure seminarioController date filter works correctly

--- src/api/seminario/controllers/seminario.js
+++ src/api/seminario/controllers/seminario.js
@@ -6,4 +6,27 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::seminario.seminario');
+module.exports = createCoreController('api::seminario.seminario', ({ strapi }) => ({
+  async find(ctx) {
+    const date =
+      ctx.query.filters && ctx.query.filters.data
+      ? ctx.query.filters.data.$contains
+      : undefined;
+
+    if(date) {
+      delete ctx.query.filters.data;
+    }
+
+    const seminarios = await super.find(ctx);
+
+    const { meta } = seminarios;
+
+    let data = seminarios.data;
+
+    if(date) {
+      data = data.filter(seminario => seminario.attributes.data.includes(date));
+    }
+
+    return { data, meta };
+  }
+}));
